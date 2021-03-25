21304fa8598554a7f76be3b39e477520a328bb6e
Haeckner <haeckner@gmail.com>
2021-03-25 10:09:58 +0100

    Restart the app from scratch for training purposes

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Sample php app
+</title>
+</head>
+<body>
+<?php echo "What a wonderful PHP app"; ?>
+</body>
+<footer>
+Copyright &copy; 2021 Me
+</footer>
+</html>
