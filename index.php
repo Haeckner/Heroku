f19185be4817f5d74cb10b3bf6c762e210e7b1de
Haeckner <haeckner@gmail.com>
2021-03-25 10:15:41 +0100

    Add Analytics tracking code

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>
-Sample php app
-</title>
+<title> Sample php app for Analytics, Ads and GTM implementation training</title>
 </head>
+  
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-192604322-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-192604322-2');
+</script>
+  
 <body>
-<?php echo "What a wonderful PHP app"; ?>
+<?php echo "The purpose of this app is purely educational. This time I am practicing implementation of Ads, Analytics and GTM."; ?>
 </body>
-<footer>
-Copyright &copy; 2021 Me
+<footer style="margin-top:2cm">
+  <center> Copyright &copy; 2021 Me </center>
 </footer>
 </html>
