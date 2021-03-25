828627cca4e2692f60e90ab7cb8ed4dbe40504fd
Haeckner <haeckner@gmail.com>
2021-03-25 11:05:26 +0100

    Added GTM snippet

--- index.php
+++ index.php
@@ -14,6 +14,14 @@
   gtag('config', 'UA-192604322-2');
 </script>
   
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WB5SBNS');</script>
+<!-- End Google Tag Manager -->
+  
 <body>
 <?php echo "The purpose of this app is purely educational. This time I am practicing implementation of Ads, Analytics and GTM."; ?>
 </body>
